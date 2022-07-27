f353021db1d11a36e87ba29f6d830116253f64ee
emmafils <imoroug@gmail.com>
2022-07-28 00:54:58 +0100

    seed user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,14 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-user = User.create(
+user = User.create!(
         name: "SPFoods Administrateur",
         email: "spfoodadmin@gmail.com",
         password: "admin12345",
         admin: true
       )
 
-user = User.create(
+user2 = User.create!(
         name: "Invité(e)",
         email: "spfoodinvite@gmail.com",
         password: "invite12345"
@@ -23,3 +23,30 @@ user = User.create(
                 password: password,
                 )
 end
+
+user3 = User.create!(
+  email: "gado@example.com",
+  password:"123456",
+  admin: false,
+)
+
+user4 = User.create!(
+  email: "gado1@example.com",
+  password:"123456",
+  admin: false,
+)
+user5 = User.create!(
+  email: "gado2@example.com",
+  password:"123456",
+  admin: false,
+)
+user6 = User.create!(
+  email: "gado3@example.com",
+  password:"123456",
+  admin: false,
+)
+user6 = User.create!(
+  email: "gado4@example.com",
+  password:"123456",
+  admin: true,
+)
