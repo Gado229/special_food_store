e996578835972059d8306015ef20dba6e9268e8a
emmafils <imoroug@gmail.com>
2022-08-08 15:50:09 +0100

    Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,90 +1,77 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-user = User.create!
-(
+user = User.create!(
         name: "SPFoods Administrateur",
         email: "spfoodadmin@gmail.com",
         password: "admin12345",
         admin: true
       )
 
-user2 = User.create!
-(
+user2 = User.create!(
         name: "Invité(e)",
         email: "spfoodinvite@gmail.com",
         password: "invite12345"
       )
 
-user3 = User.create!
-(
+user3 = User.create!(
   email: "gado@example.com",
   password:"123456",
   admin: false,
 )
 
-user4 = User.create!
-(
+user4 = User.create!(
   email: "gado1@example.com",
   password:"123456",
   admin: false,
 )
-user5 = User.create!
-(
+user5 = User.create!(
   email: "gado2@example.com",
   password:"123456",
   admin: false,
 )
-user6 = User.create!
-(
+user6 = User.create!(
   email: "gado3@example.com",
   password:"123456",
   admin: false,
 )
-user6 = User.create!
-(
+user6 = User.create!(
   email: "gado4@example.com",
   password:"123456",
   admin: true,
 )
 
-restaurant = Restaurant.create!
-(
+restaurant = Restaurant.create!(
   name: "Les Capsules",
   adress: "Zopa, près du CS La Grande Academie",
   phone: "96775656"
 )
 
-restaurant1 = Restaurant.create!
-(
+restaurant1 = Restaurant.create!(
   name: "Chawarma Express",
   adress: "Abomez-calavi, face à l'entrée principale du Campus UAC",
   phone: "96000006"
 )
 
-restaurant2 = Restaurant.create!
-(
+restaurant2 = Restaurant.create!(
   name: "Chez Tanti",
   adress: "Zopa, près du CS La Grande Academie",
   phone: "96000007"
 )
 
-restaurant3 = Restaurant.create!
-(
+restaurant3 = Restaurant.create!(
   name: "Chez Maman Gracia",
   adress: "Face au Petit Portail du CEG1 Abomey-calavi",
   phone: "96000008"
 )
 
-restaurant4 = Restaurant.create!
-(
+restaurant4 = Restaurant.create!(
   name: "Le Chef",
-  adress: "Zopa, près du CS La Grande Academie",
+  adress: "Mènontin",
   phone: "96000009"
 )
 
-restaurant5 = Restaurant.create!
-(
+restaurant5 = Restaurant.create!(
   name: "Le Bénin",
   adress: "Maromilitaire Rue derrière CINÉ LE BENIN",
   phone: "96000010"
@@ -94,11 +81,11 @@ restaurant = ['Les Capsules', 'Chawarma Express', 'Chez Tanti', 'Chez Maman Grac
 
 
 50.times do |i|
-    Food.create(
+    Food.create!(
         name: "Plat#{i + 1}",
   restaurant: restaurant[rand(3)],
  description: "C'est la description du plat#{i + 1}",
         prix: "2#{i + 500}",
-     user_id: rand(2..48)
+     user_id: rand(10..48)
     )
 end
